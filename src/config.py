903414d9f224bbd1d3ee5427e8f83deb32dcce4a
Jonasz Delimata <jonasz.dl@gmail.com>
2026-02-28 12:12:49 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,8 +5,14 @@ PROJECT_ROOT = Path(__file__).parent.parent
 
 # Third-party tools
 SERVER_DIR = PROJECT_ROOT / "tmp" / "third-party" / "server"
-CHUNKER_BIN = PROJECT_ROOT / "tmp" / "third-party" / "chunker-cli" / "chunker-cli.exe"
-MCMAP_BIN = PROJECT_ROOT / "tmp" / "third-party" / "mcmap" / "mcmap.exe"
+
+# MacOS
+CHUNKER_BIN = PROJECT_ROOT / "tmp" / "third-party" / "chunker-cli" / "bin" / "chunker-cli"
+MCMAP_BIN = PROJECT_ROOT / "tmp" / "third-party" / "mcmap" / "build" / "bin" / "mcmap"
+
+# Windows
+# CHUNKER_BIN = PROJECT_ROOT / "tmp" / "third-party" / "chunker-cli" / "chunker-cli.exe"
+# MCMAP_BIN = PROJECT_ROOT / "tmp" / "third-party" / "mcmap" / "mcmap.exe"
 
 # Data Processing
 PROCESSED_WORLDS_DIR = PROJECT_ROOT / "tmp" / "processed_worlds"
